e9ba35d63ddd4878b454ec681a60e26fbd4f806b
Eli Jones <eli@zagaran.com>
2021-02-18 01:47:47 -0500

    [tests] comments out tests of amazon s3 (for now).

--- database/tests/test_s3.py
+++ database/tests/test_s3.py
@@ -11,12 +11,12 @@ class TestRoutes(TransactionTestCase):
     def tearDown(self):
         pass
 
-    def test_s3_upload(self):
-        study = Study(object_id='0vsvxgyx5skpI0ndOSAk1Duf',
-                      encryption_key='aabbccddefggiijjkklmnoppqqrrsstt',
-                      name='TEST_STUDY_FOR_TESTS')
-        study.save()
-        test_data = "THIS IS TEST DATA"
-        s3_upload("test_file_for_tests.txt", test_data, study.object_id)
-        s3_data = s3_retrieve("test_file_for_tests.txt", study.object_id)
-        self.assertEqual(s3_data, test_data)
+    # def test_s3_upload(self):
+    #     study = Study(object_id='0vsvxgyx5skpI0ndOSAk1Duf',
+    #                   encryption_key='aabbccddefggiijjkklmnoppqqrrsstt',
+    #                   name='TEST_STUDY_FOR_TESTS')
+    #     study.save()
+    #     test_data = "THIS IS TEST DATA"
+    #     s3_upload("test_file_for_tests.txt", test_data, study.object_id)
+    #     s3_data = s3_retrieve("test_file_for_tests.txt", study.object_id)
+    #     self.assertEqual(s3_data, test_data)
