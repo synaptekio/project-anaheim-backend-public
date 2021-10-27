21609ccab5ccbd5d728fb4c7d8dbfe0760e557c5
Eli Jones <biblicabeebli@gmail.com>
2021-10-26 20:09:32 -0400

    [django] Didn't know we had this profiler mode, but I've checked and its not django compatible

--- profiler.py
+++
@@ -1,7 +0,0 @@
-# Run python profiler.py to see performance and bottlenecks on page loads and API calls
-from werkzeug.middleware.profiler import ProfilerMiddleware
-from app import app
-
-app.config['PROFILE'] = True
-app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[30])
-app.run(debug = True)
