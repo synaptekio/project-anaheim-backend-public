362cbf3c66c7a9a844af684e6a4ef27e542f2e30
Eli Jones <biblicabeebli@gmail.com>
2021-11-04 23:17:14 -0400

    [django] random migration improvement while doing other things

--- database/migrations/0024_custom.py
+++ database/migrations/0024_custom.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.db import migrations
 
-from database.user_models import Researcher
-
 
 def convert_batch_users(apps, schema_editor):
+    Researcher = apps.get_model('database', 'Researcher')
     for researcher in Researcher.objects.all():
         if researcher.username.startswith("BATCH USER"):
             researcher.is_batch_user = True
