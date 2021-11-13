5b94960d83b2193d0427fa017095bc8a59d96fed
Eli Jones <biblicabeebli@gmail.com>
2021-11-12 21:23:26 -0500

    [django] copypasta test...

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -905,3 +905,14 @@ class TestDataAccessWebFormPage(GeneralPageTest):
         id_key, secret_key = self.session_researcher.reset_access_credentials()
         resp = self.smart_get()
         self.assert_not_present("Reset Data-Download API Access Credentials", resp.content)
+
+
+class TestPipelineWebFormPage(GeneralPageTest):
+    ENDPOINT_NAME = "data_access_web_form.pipeline_download_page"
+    
+    def test(self):
+        resp = self.smart_get()
+        self.assert_present("Reset Data-Download API Access Credentials", resp.content)
+        id_key, secret_key = self.session_researcher.reset_access_credentials()
+        resp = self.smart_get()
+        self.assert_not_present("Reset Data-Download API Access Credentials", resp.content)
