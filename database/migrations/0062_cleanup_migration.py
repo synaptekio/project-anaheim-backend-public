cdc6563da2724fd3ec855d16bf4f99a8e8911edb
Eli Jones <biblicabeebli@gmail.com>
2021-12-02 16:39:08 -0500

    [django] allows cleanup migration to be reversable

--- database/migrations/0062_cleanup_migration.py
+++ database/migrations/0062_cleanup_migration.py
@@ -7,6 +7,9 @@ def remove_batch_users(apps, schema_editor):
     Researcher = apps.get_model('database', 'Researcher')
     Researcher.objects.filter(is_batch_user=True).delete()
 
+def remove_batch_users_in_reverse(apps, schema_editor):
+    pass
+
 
 class Migration(migrations.Migration):
     
@@ -25,7 +28,7 @@ class Migration(migrations.Migration):
             name='survey_type',
             field=models.CharField(choices=[('audio_survey', 'audio_survey'), ('tracking_survey', 'tracking_survey'), ('image_survey', 'image_survey')], help_text='What type of survey this is.', max_length=16),
         ),
-        migrations.RunPython(remove_batch_users),
+        migrations.RunPython(remove_batch_users, reverse_code=remove_batch_users_in_reverse),
         migrations.AlterField(
             model_name='participant',
             name='push_notification_unreachable_count',
