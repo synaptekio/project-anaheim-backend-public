397a066e486c520e645704abeaa92f943bcdf58c
Eli Jones <biblicabeebli@gmail.com>
2022-02-14 04:04:01 -0500

    [dashboard-refactor] removes a test!

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -932,17 +932,6 @@ class TestDataAccessWebFormPage(ResearcherSessionTest):
         self.assert_not_present("Reset Data-Download API Access Credentials", resp.content)
 
 
-class TestPipelineWebFormPage(ResearcherSessionTest):
-    ENDPOINT_NAME = "data_access_web_form.pipeline_download_page"
-    
-    def test(self):
-        resp = self.smart_get()
-        self.assert_present("Reset Data-Download API Access Credentials", resp.content)
-        id_key, secret_key = self.session_researcher.reset_access_credentials()
-        resp = self.smart_get()
-        self.assert_not_present("Reset Data-Download API Access Credentials", resp.content)
-
-
 # FIXME: add error cases to this test
 class TestSetStudyTimezone(RedirectSessionApiTest):
     ENDPOINT_NAME = "admin_api.set_study_timezone"
