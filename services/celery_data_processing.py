04ac4ae2fe5bbb504eb1c64f6947d6c4c3056f88
Eli Jones <biblicabeebli@gmail.com>
2022-02-23 01:35:42 -0500

    [data-processing-updates] cleanup

--- services/celery_data_processing.py
+++ services/celery_data_processing.py
@@ -23,11 +23,11 @@ def create_file_processing_tasks():
     Also, for some reason 5 minutes is the smallest value that .... works.  At all.
     No clue why.
     """
-
+    
     # set the tasks to expire at the 5 minutes and thirty seconds mark after the most recent
     # 6 minutely cron task. This way all tasks will be revoked at the same, and well-known, instant.
     expiry = (datetime.utcnow() + timedelta(minutes=5)).replace(second=30, microsecond=0)
-
+    
     with make_error_sentry(sentry_type=SentryTypes.data_processing):
         participant_set = set(
             Participant.objects.filter(files_to_process__isnull=False)
@@ -36,13 +36,13 @@ def create_file_processing_tasks():
                 .order_by("?")     # don't want a single user blocking everyone because they are at the front.
                 .values_list("id", flat=True)
         )
-
+        
         # sometimes celery just fails to exist, set should be redundant.
         active_set = set(get_processing_active_job_ids())
-
+        
         participants_to_process = participant_set - active_set
         print("Queueing these participants:", ",".join(str(p) for p in participants_to_process))
-
+        
         for participant_id in participants_to_process:
             # Queue all users' file processing, and generate a list of currently running jobs
             # to use to detect when all jobs are finished running.
@@ -62,23 +62,24 @@ def create_file_processing_tasks():
 def celery_process_file_chunks(participant_id):
     """ This is the function is queued up, it runs through all new uploads from a specific user and
     'chunks' them. Handles logic for skipping bad files, raising errors. """
-
+    
     # celery doesn't clean up after itself very well, either memory or open network connections.
     # this probably has something to do with the fact that celery forks, so possibly picking
     # a different mode would impact this.  Or we can just exit the python process.
     try:
         time_start = datetime.now()
         participant = Participant.objects.get(id=participant_id)
-
+        
         number_bad_files = 0
-        tags = {'user_id': participant.patient_id}
-        error_sentry = make_error_sentry(sentry_type=SentryTypes.data_processing)
+        error_sentry = make_error_sentry(
+            sentry_type=SentryTypes.data_processing, tags={'user_id': participant.patient_id}
+        )
         print("processing files for %s" % participant.patient_id)
-
+        
         while True:
             previous_number_bad_files = number_bad_files
             starting_length = participant.files_to_process.exclude(deleted=True).count()
-
+            
             print("%s processing %s, %s files remaining" % (datetime.now(), participant.patient_id, starting_length))
             number_bad_files += do_process_user_file_chunks(
                     page_size=FILE_PROCESS_PAGE_SIZE,
@@ -95,7 +96,7 @@ def celery_process_file_chunks(participant_id):
                     break
                 else:
                     continue
-
+            
             # put maximum time limit per user
             if (time_start - datetime.now()).total_seconds() > 60*60*3:
                 break
