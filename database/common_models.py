c1fcdce9eae6ea4d60b7b66bfa0f8c6420c2db47
Eli Jones <biblicabeebli@gmail.com>
2022-03-03 02:46:06 -0500

    [main] adds convenience function.

--- database/common_models.py
+++ database/common_models.py
@@ -25,7 +25,13 @@ class JSONTextField(models.TextField):
 class UtilityModel(models.Model):
     """ Provides numerous utility functions and enhancements.
         All Models should subclass UtilityModel. """
-
+    
+    @classmethod
+    def nice_count(cls):
+        count = cls.objects.count()
+        print("{:,}".format(count))
+        return count
+    
     @classmethod
     def generate_objectid_string(cls, field_name):
         """
@@ -35,32 +41,32 @@ class UtilityModel(models.Model):
         string of exactly 24 characters.  The value must be typeable, and special characters
         should be avoided.
         """
-
+        
         for _ in range(10):
             object_id = generate_objectid_string()
             if not cls.objects.filter(**{field_name: object_id}).exists():
                 break
         else:
             raise ObjectIdError("Could not generate unique id for %s." % cls.__name__)
-
+        
         return object_id
-
+    
     @classmethod
     def get_or_404(cls, *args, **kwargs):
         try:
             return cls.objects.get(*args, **kwargs)
         except cls.DoesNotExist:
             return abort(404)
-
+    
     def as_dict(self):
         """ Provides a dictionary representation of the object """
         return {field.name: getattr(self, field.name) for field in self._meta.fields}
-
+    
     @property
     def _contents(self):
         """ Convenience purely because this is the syntax used on some other projects """
         return self.as_dict()
-
+    
     @property
     def _related(self):
         """ Gets all related objects for this database object (warning: probably huge).
@@ -72,12 +78,12 @@ class UtilityModel(models.Model):
             # There is no predictable way to access related models that do not have related names.
             # ... unless there is a way to inspect related_field.related_model._meta._relation_tree
             # and determine the field relationship to then magically create a query? :D
-
+            
             # one to one fields use this...
             if related_field.one_to_one and related_field.related_name:
                 related_entity = getattr(self, related_field.related_name)
                 ret[related_field.related_name] = related_entity.as_dict() if related_entity else None
-
+            
             # many to one and many to many use this.
             elif related_field.related_name:
                 # get all the related things using .values() for access, but convert to dict
@@ -86,16 +92,16 @@ class UtilityModel(models.Model):
                 db_calls += 1
                 ret[related_field.related_name] = [x for x in related_manager.all().values()]
                 entities_returned += len(ret[related_field.related_name])
-
+        
         return ret
-
+    
     @property
     def _everything(self):
         """ Gets _related and _contents. Will probably be huge. Debugging only. """
         ret = self._contents
         ret.update(self._related)
         return ret
-
+    
     def as_unpacked_native_python(self, remove_timestamps=True) -> dict:
         """
         Collect all of the fields of the model and return their values in a python dict,
@@ -116,14 +122,14 @@ class UtilityModel(models.Model):
             else:
                 # Otherwise, just return the field's value directly
                 field_dict[field_name] = getattr(self, field_name)
-
+        
         return field_dict
-
+    
     def save(self, *args, **kwargs):
         # Raise a ValidationError if any data is invalid
         self.full_clean()
         super().save(*args, **kwargs)
-
+    
     def update(self, **kwargs):
         """ Convenience method on to update the database with a dictionary or kwargs."""
         for attr, value in kwargs.items():
@@ -132,7 +138,7 @@ class UtilityModel(models.Model):
                 raise Exception(f"unpexpected parameter: {attr}")
             setattr(self, attr, value)
         self.save()
-
+    
     def __str__(self):
         """ multipurpose object representation """
         if hasattr(self, 'study'):
@@ -141,7 +147,7 @@ class UtilityModel(models.Model):
             return f'{self.__class__.__name__} {self.name}'
         else:
             return f'{self.__class__.__name__} {self.pk}'
-
+    
     class Meta:
         abstract = True
 
@@ -150,6 +156,6 @@ class TimestampedModel(UtilityModel):
     """ TimestampedModels record last access and creation time. """
     created_on = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
-
+    
     class Meta:
         abstract = True
